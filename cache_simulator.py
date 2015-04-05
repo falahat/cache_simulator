fc39701d109c18667c3af09bc37a7d79e279a328
Aryan Falahatpisheh <aryan.falahatpisheh@berkeley.edu>
2015-04-05 15:59:34 -0700

    Going to add classes for sets and blocks

--- cache_simulator.py
+++ cache_simulator.py
@@ -1,6 +1,16 @@
 
 class Cache(Object):
 	def __init__(cache_size, N_way, bytes_per_entry):
+
+		# Given
 		self.cache_size = cache_size
 		self.N_way = N_way
 		self.bytes_per_entry = bytes_per_entry
+
+		# Calculating Sizes
+		self.num_entries = cache_size / bytes_per_entry
+		self.num_sets = num_entries / N_way
+		self.num_ways = self.num_entries / self.num_sets
+
+
+		self.sets = map(); # {Set Index => Cache Blocks} but in a set object
