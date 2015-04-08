8dc146ecfac549bf234cf82a72f1dc535a6d5c0d
Aryan Falahatpisheh <aryan.falahatpisheh@berkeley.edu>
2015-04-08 01:03:43 -0700

    Making sample program not look horrible

--- sample_program.py
+++ sample_program.py
@@ -1,7 +1,6 @@
 
 from cache_simulator import Memory, Cache
 
-
 # Address length
 memory = Memory(32);
 
@@ -26,27 +25,6 @@ for j in range(step):
     a = lookup(addr_a_original)
     b = lookup(addr_b_original)
     c = write(addr_c_original, a + b)
-
-
-    addr_a = bin(addr_a_original)
-    addr_b = bin(addr_b_original)
-    addr_c = bin(addr_c_original)
-
-    # print("i: " + str(i) + " j: " + str(j) + " -+-"*10)
-    # print("A address\t\tresponse\tB address\t\tresponse\tc response")
-    # print(addr_a + " : " + a[1] + "\t" + addr_b + " : " + b[1] + "\t" + c[1])
-    # addr_a = ""
-    # for el in list(cache.tokenize(addr_a_original)):
-    #     if type(el) == type(1):
-    #         el = bin(el)[2:]
-    #     addr_a += "|" + str(el) +  "|" 
-    # # print("A:  " + addr_a)
-    # addr_b = ""
-    # for el in list(cache.tokenize(addr_b_original)):
-    #     if type(el) == type(1):
-    #         el = bin(el)[2:]
-    #     addr_b +=   "|" + str(el) + "|";
-    # # print("B:  " + addr_b)
 
 
 cache.dump_info()
